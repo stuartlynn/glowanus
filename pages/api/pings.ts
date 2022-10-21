02ec1c50bdd2ff4ed91d2068429c737044ddb15e
Stuart Lynn <stuart.lynn@gmail.com>
2022-10-21 17:36:44 -0400

    recording ping response

--- pages/api/pings.ts
+++ pages/api/pings.ts
@@ -4,13 +4,9 @@ import {PrismaClient} from "@prisma/client"
 
 const prisma = new PrismaClient()
 
-type Data = {
-  name: string
-}
-
 export  default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   
   const pings = await  prisma.ping.findMany();
--- pages/api/record_ping.ts
+++ pages/api/record_ping.ts
@@ -4,17 +4,15 @@ import {PrismaClient} from "@prisma/client"
 
 const prisma = new PrismaClient()
 
-type Data = {
-  name: string
-}
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
 
   const query = req.query;
   const { deviceId } = query;
+  
   
   const newPing = await prisma.ping.create({
     data:{
