e85daa7f867c85874f4abd42769d0fe170e0767d
Stuart Lynn <stuart.lynn@gmail.com>
2023-02-07 23:43:50 -0500

    code reorg

--- prisma/migrations/20221021130049_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Ping" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "deviceId" VARCHAR(255) NOT NULL,
-
-    CONSTRAINT "Ping_pkey" PRIMARY KEY ("id")
-);
